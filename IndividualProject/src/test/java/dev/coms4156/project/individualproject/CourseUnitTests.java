5784325af1a7ace34cd1a5f3445ae45468853194
Ella Kim <74640491+ellayunjikim@users.noreply.github.com>
2024-09-05 14:24:01 -0400

    Add Course unit test  (#3)

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -30,6 +30,71 @@ public class CourseUnitTests {
     assertEquals(expectedResult, testCourse.toString());
   }
 
+  @Test
+  public void enrollStudentTest() {
+    testCourse.setEnrolledStudentCount(250);
+    assertEquals(false, testCourse.enrollStudent());
+    testCourse.setEnrolledStudentCount(100);
+    assertEquals(true, testCourse.enrollStudent());
+  }
+
+  @Test
+  public void dropStudentTest() {
+    testCourse.setEnrolledStudentCount(240);
+    assertEquals(true, testCourse.dropStudent());
+    testCourse.setEnrolledStudentCount(0);
+    assertEquals(false, testCourse.dropStudent());
+  }
+
+  @Test
+  public void isCourseFullTest() {
+    testCourse.setEnrolledStudentCount(0);
+    assertEquals(false, testCourse.isCourseFull());
+    testCourse.setEnrolledStudentCount(250);
+    assertEquals(true, testCourse.isCourseFull());
+    testCourse.setEnrolledStudentCount(251);
+    assertEquals(true, testCourse.isCourseFull());
+  }
+
+  @Test
+  public void getCourseLocationTest() {
+    String expectedResult = "417 IAB";
+    assertEquals(expectedResult, testCourse.getCourseLocation());
+  }
+
+  @Test
+  public void getInstructorNameTest() {
+    String expectedResult = "Griffin Newbold";
+    assertEquals(expectedResult, testCourse.getInstructorName());
+  }
+
+  @Test
+  public void getCourseTimeSlotTest() {
+    String expectedResult = "11:40-12:55";
+    assertEquals(expectedResult, testCourse.getCourseTimeSlot());
+  }
+
+  @Test
+  public void reassignInstructorTest() {
+    String newInstructor = "Ella Kim";
+    testCourse.reassignInstructor(newInstructor);
+    assertEquals(newInstructor, testCourse.getInstructorName());
+  }
+
+  @Test
+  public void reassignLocationTest() {
+    String newLocation = "CSB 451";
+    testCourse.reassignLocation(newLocation);
+    assertEquals(newLocation, testCourse.getCourseLocation());
+  }
+
+  @Test
+  public void reassignTimeTest() {
+    String newTime = "1:00-2:20";
+    testCourse.reassignTime(newTime);
+    assertEquals(newTime, testCourse.getCourseTimeSlot());
+  }
+
   /** The test course instance used for testing. */
   public static Course testCourse;
 }
